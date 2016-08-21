1106a320710cf9e96c0078d9530f73eda217b5ad
Jordan Peck <jordan.me2@gmail.com>
2016-08-21 23:10:19 +0100

    License files and headers

--- FastNoiseSIMD_JNI.cpp
+++ FastNoiseSIMD_JNI.cpp
@@ -1,3 +1,21 @@
+/*
+StarMade Native Library
+Copyright(C) 2016 Schine, GmbH
+
+This program is free software : you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "FastNoiseSIMD_JNI.h"
 #include "FastNoiseSIMD/FastNoiseSIMD.h"
 #include <string.h>
--- main.cpp
+++ main.cpp
@@ -8,10 +8,9 @@
 
 using namespace std;
 
+// For debug only, don't include in release
 int _tmain(int argc, _TCHAR* argv[])
 {
-	//auto pv = Java_org_schema_game_server_controller_world_factory_planet_FastNoiseSIMD_NewVectorSet(nullptr, nullptr, 0, new jfloatArray());
-
 	auto p = Java_org_schema_game_server_controller_world_factory_planet_FastNoiseSIMD_NewFastNoiseSIMD(nullptr, nullptr, 1234);
 	Java_org_schema_game_server_controller_world_factory_planet_FastNoiseSIMD_NativeSetFrequency(nullptr, nullptr,p, 0.0008f);
 	Java_org_schema_game_server_controller_world_factory_planet_FastNoiseSIMD_NativeSetSeed(nullptr, nullptr, p, 69);
